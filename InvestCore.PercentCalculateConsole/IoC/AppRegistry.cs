df7e5b36141d979480c9a376e8a47c4aa7119e1d
Hattler <ivan.1.99@mail.ru>
2023-02-28 20:32:43 +0400

    move startup functions to appregistry

---
+++ InvestCore.PercentCalculateConsole/IoC/AppRegistry.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using Autofac;
+using InvestCore.Domain.Services.Implementation;
+using InvestCore.Domain.Services.Interfaces;
+using InvestCore.PercentCalculateConsole.Services.Implementation;
+using InvestCore.PercentCalculateConsole.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Tinkoff.InvestApi;
+
+namespace InvestCore.PercentCalculateConsole.IoC
+{
+    public static class AppRegistry
+    {
+        public static IConfigurationRoot BuildConfig()
+        {
+            return new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddUserSecrets(Assembly.GetExecutingAssembly())
+                .AddJsonFile("config.json", false)
+                .Build();
+        }
+
+        public static IContainer BuildContainer(string tinkoffToken)
+        {
+            var builder = new ContainerBuilder();
+
+            #region Logger
+
+            builder.Register(c =>
+                LoggerFactory.Create(x =>
+                {
+                    x.AddConsole();
+                    x.AddDebug();
+                }).CreateLogger<ILogger>())
+                .SingleInstance().As<ILogger>();
+
+            #endregion Logger
+
+            #region Services
+
+            builder.RegisterType<SelectBestBuyModelStrategyByMul>().SingleInstance().As<ISelectBestBuyModelStrategy>();
+            builder.RegisterType<BuyModelService>().SingleInstance().As<IBuyModelService>();
+            builder.RegisterType<FakeShareService>().SingleInstance().As<IShareService>();
+            builder.RegisterType<StockPortfolioService>().SingleInstance().As<IStockPortfolioService>();
+            builder.Register(c => InvestApiClientFactory.Create(tinkoffToken)).SingleInstance();
+            builder.RegisterType<MessageService>().SingleInstance().As<IMessageService>();
+
+            #endregion Services
+
+            return builder.Build();
+        }
+    }
+}
--- InvestCore.PercentCalculateConsole/Program.cs
+++ InvestCore.PercentCalculateConsole/Program.cs
@@ -1,77 +1,23 @@
-﻿using System.Reflection;
-using Autofac;
-using InvestCore.Domain.Services.Implementation;
-using InvestCore.Domain.Services.Interfaces;
+﻿using Autofac;
 using InvestCore.PercentCalculateConsole.Domain;
-using InvestCore.PercentCalculateConsole.Services.Implementation;
+using InvestCore.PercentCalculateConsole.IoC;
 using InvestCore.PercentCalculateConsole.Services.Interfaces;
-using InvestCore.TinkoffApi.Services;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Tinkoff.InvestApi;
 
 internal class Program
 {
-    private static readonly IContainer _container;
-    private static readonly IConfigurationRoot _configuration;
-    private static readonly StockPortfolioCalculationModel _stockPortfolio;
-    private static readonly IMessageService _messageService;
-
-    static Program()
+    private static void Main()
     {
-        _configuration = BuildConfig();
-
-        var telegramToken = _configuration.GetRequiredSection("TinkoffToken").Get<string>()
+        var configuration = AppRegistry.BuildConfig();
+        var telegramToken = configuration.GetRequiredSection("TinkoffToken").Get<string>()
             ?? string.Empty;
-        _stockPortfolio = _configuration.GetRequiredSection("StockPortfolioCalculationModel").Get<StockPortfolioCalculationModel>()
+        var stockPortfolio = configuration.GetRequiredSection("StockPortfolioCalculationModel").Get<StockPortfolioCalculationModel>()
             ?? new StockPortfolioCalculationModel();
+        var container = AppRegistry.BuildContainer(telegramToken);
 
-        _container = BuildContainer(telegramToken);
-        _messageService = _container.Resolve<IMessageService>();
-    }
 
-    private static void Main(string[] args)
-    {
-        Console.WriteLine(_messageService.GetResultMessage(_stockPortfolio));
+        var messageService = container.Resolve<IMessageService>();
+        Console.WriteLine(messageService.GetResultMessage(stockPortfolio));
         Console.ReadLine();
-    }
-
-    private static IConfigurationRoot BuildConfig()
-    {
-        return new ConfigurationBuilder()
-            .AddEnvironmentVariables()
-            .AddUserSecrets(Assembly.GetExecutingAssembly())
-            .AddJsonFile("config.json", false)
-            .Build();
-    }
-
-    private static IContainer BuildContainer(string tinkoffToken)
-    {
-        var builder = new ContainerBuilder();
-
-        #region Logger
-
-        builder.Register(c =>
-            LoggerFactory.Create(x =>
-            {
-                x.AddConsole();
-                x.AddDebug();
-            }).CreateLogger<ILogger>())
-            .SingleInstance().As<ILogger>();
-
-        #endregion Logger
-
-        #region Services
-
-        builder.RegisterType<SelectBestBuyModelStrategyByMul>().SingleInstance().As<ISelectBestBuyModelStrategy>();
-        builder.RegisterType<BuyModelService>().SingleInstance().As<IBuyModelService>();
-        builder.RegisterType<FakeShareService>().SingleInstance().As<IShareService>();
-        builder.RegisterType<StockPortfolioService>().SingleInstance().As<IStockPortfolioService>();
-        builder.Register(c => InvestApiClientFactory.Create(tinkoffToken)).SingleInstance();
-        builder.RegisterType<MessageService>().SingleInstance().As<IMessageService>();
-
-        #endregion Services
-
-        return builder.Build();
     }
 }
