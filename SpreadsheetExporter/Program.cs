192c990b6b7c7b8a0afd99bb7d5655bc7331d59f
Hattler <ivan.1.99@mail.ru>
2023-03-26 22:16:58 +0400

    timer processing

--- SpreadsheetExporter/Program.cs
+++ SpreadsheetExporter/Program.cs
@@ -1,10 +1,11 @@
 ﻿using Autofac;
 using InvestCore.Domain.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using PercentCalculateConsole.IoC;
 using SpreadsheetExporter.Domain;
 using SpreadsheetExporter.Services.Interfaces;
-
+using Timer = System.Threading.Timer;
 
 var configuration = AppRegistry.BuildConfig();
 var telegramToken = configuration.GetRequiredSection("TinkoffToken").Get<string>()
@@ -23,4 +24,14 @@ var container = AppRegistry.BuildContainer(telegramToken, spreadsheetConfig);
 
 var workflowService = container.Resolve<IWorkflowService>();
 
-await workflowService.UpdateTableAsync(tickerInfos, spreadsheetConfig, replenisment, portfolioInvestment);
+var logger = container.Resolve<ILogger>();
+#pragma warning disable AsyncFixer03 // Fire-and-forget async-void methods or delegates
+var callback = new TimerCallback(async state =>
+{
+    await workflowService
+        .UpdateTableAsync(tickerInfos, spreadsheetConfig, replenisment, portfolioInvestment);
+    logger.LogInformation("---------------------Окончание обработки---------------------");
+});
+#pragma warning restore AsyncFixer03 // Fire-and-forget async-void methods or delegates
+var timer = new Timer(callback, null, 0, 60*60*1000);
+Console.ReadLine();
